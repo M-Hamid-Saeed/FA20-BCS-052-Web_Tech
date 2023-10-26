337b30e4a5232bc3ed5c04272304b7258eea442e
M-Hamid-Saeed <hamid.saeed.uni@gmail.com>
2023-10-26 16:04:51 +0500

    ..

---
+++ Practice/colaScript.js
@@ -0,0 +1,7 @@
+function handleRadioClick(imageName) {
+  // Get the radio button element
+  const radioElement = document.getElementById("image");
+
+  radioElement.src = imageName;
+}
+console.log("HELLO WORLD");
