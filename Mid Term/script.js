57a3f1737e8447098638b793bbe22162706d7cac
M-Hamid-Saeed <hamid.saeed.uni@gmail.com>
2023-11-17 16:56:08 +0500

    .

--- Mid Term/script.js
+++ Mid Term/script.js
@@ -16,3 +16,33 @@ function showSlides() {
   slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+let data = null;
+$.ajax({
+  method: "get",
+  url: "",
+  success: function (response) {
+    data = response.data;
+    $("#table").append();
+  },
+});
+
+$.ajax({
+  method: "get",
+  url: "adjf",
+})
+  .then((res) => {
+    console.log("res", res);
+  })
+  .catch((err) => {
+    console.log("error");
+  });
+
+const a = async () => {
+  console.log("my name is ahmad");
+};
+
+a.then(() => {
+  console.log("success");
+}).catch(() => {
+  console.log("errror");
+});
